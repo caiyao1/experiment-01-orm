68363f7c2bf86dabbb7d567916cd8d36b389ab2c
caiyao1 <2857700368@qq.com>
2019-03-14 14:29:01 +0800

    update 2019.03.14 User Address

---
+++ src/main/java/com/example/experiment01orm/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package com.example.experiment01orm.repository;
+
+import com.example.experiment01orm.entity.User;
+import com.example.experiment01orm.entity.Address;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+@Transactional
+public class UserRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void addUserAddress() {
+        User user = new User("CY");
+        em.persist(user);
+        Address address1 = new Address("956");
+        address1.setUser(user);
+        em.persist(address1);
+
+        Address address2 = new Address("925");
+        address2.setUser(user);
+        em.persist(address2);
+    }
+}
